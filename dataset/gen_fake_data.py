b286254e5e6059325d7593aeda14443bcae1929c
KeleiHe <34135925+KeleiHe@users.noreply.github.com>
2020-10-20 23:19:49 +0800

    Update gen_fake_data.py

--- dataset/gen_fake_data.py
+++ dataset/gen_fake_data.py
@@ -11,7 +11,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from dataset.WebCaricature import WebCaricature
+from dataset.WebCariA import WebCariA
 from dataloader import *
 from model.GAN.UGATIT import UGATIT
 from utils.gan import *
